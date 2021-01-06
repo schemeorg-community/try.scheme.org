c32aa30c76e37e0587b35445d7284f5400f017c4
Lassi Kortela <lassi@lassi.io>
2021-01-06 15:29:46 +0200

    Add `make codemirror` and `make upload`

---
+++ try/manifest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+echo Console.js
+echo Interpreter.min.js
+echo Interpreter.scm
+echo console.css
+echo index.html
+grep -oE 'codemirror.*.(css|js)' index.html
+find lib -type f -name "*.js"
